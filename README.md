f4f2c17c9a084e6fe4ee7ce83775e2dba91dfc76
Joao Falk <joao.gabriel.falk@gmail.com>
2022-06-01 21:25:08 -0300

    feat: added comments and readme explaining code and thought process

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# email-validation-api
+This is a simple API to receive an email value and return a JSON with the following properties:
+
+{
+raw_email: string,
+cleansed_email: string,
+domain: string,
+validation_timestamp: string,
+domain_valid_indicator: string,
+username_valid_indicator: string,
+validation_result: string
+}
+
+Thought process:
+
+When I think of matching patterns I usually think of regex, so that's what I ended up using throughout the whole API. Took me about 2h to code it and then another 30-40 min writing the comments explaining what each function does, the rest of the time was spent checking if any adjustments were needed and testing.
+My solution and intent is better explained in the actual code with comments, I did it there because I thought it would be easier to visualize and show examples.
+
+The hardest part for me was getting all those regexes to work, I like using them but their syntax is a pain to remember, there were some problems at the beginning because I forgot to add the g for global in them and ended up only removing the first match in the substitutions.
+
+Technologies used:
+Typescript - I like using it and I think it is a really powerfull tool to help when writing code;
+Node and Express - for the communication and the actual routing of the API.
+
+To run:
+1 - run "npm install" to install all dependencies;
+2 - run "npm build" to build and compile the code;
+3 - run "npm run dev" to actually run the API.
+
+To use it you have to make a request to your local api ("http://localhost:3333") with a body containing a JSON with the following properties:
+{
+"email": string
+}
--- cleanEmailService.ts
+++ cleanEmailService.ts
@@ -7,50 +7,75 @@ interface ReturnInterface {
   username_valid_indicator: string;
 }
 
+// This function cleans all comments from the emaiil value
 const cleanComments = (email: string) => {
   const cleanCommentRegex = /(\(.*\))|(\<.*\>)/g;
-  // This regex uses .* to match any characters between () or <>
-  // .* is equivalent to all characters
+  // This regex uses .* to match any characters between () or <>;
+  // .* is equivalent to all characters;
+  // The g at the end indicates that it should not stop after the first match
+
   return email.replace(cleanCommentRegex, "");
+  // Than we use a simple replace function from base javascript to replace all values matched in the following regex with an empty string, removing them
 };
 
+// This function cleans the + sign and all characters following it until the @ symbol in the email value
 const cleanPlusSign = (email: string) => {
   const cleanPlusSignRegex = /(\+)(.*)(?=@)/g;
-  // This regex uses .* to match any character between the + and the @, without including the latter thanks to the ?= operator
-  // ?= excludes the following character from the matched group
+  // This regex uses .* to match any character between the + and the @, without including the latter thanks to the ?= operator;
+  // ?= excludes the character following it from the matched group;
   // .* is equivalent to all characters
+
   return email.replace(cleanPlusSignRegex, "");
+  // Than we use a simple replace function from base javascript to replace all values matched in the regex above with an empty string, removing them
 };
 
+// This function cleans all dots before the domain
 const cleanDotBeforeAt = (email: string) => {
   return (
-    email.slice(0, email.indexOf("@")).replace(".", "") +
+    email.slice(0, email.indexOf("@")).replace(/[.]/g, "") +
     email.slice(email.indexOf("@"))
   );
+  // Using a slice (from start -> @ not including the @) function to separate the username (before the at) and then a replace remove all dots from it,
+  // and combines with the domain later using another slice (from @ -> end)
 };
 
+// This function cleans all withespaces from the email value
 const cleanWhiteSpace = (email: string) => {
-  return email.replace(" ", "");
+  return email.replace(/ /g, "");
+  // Using a simple replace function from base javascript to replace all whitespace characters with an empty string
 };
 
+// This function validates the domain
 const isDomainValid = (domain: string) => {
   const validCharactersRegex = /[^\w.-]/g;
   // The above regex matches all characters not in the group;
   // \w is equivalent to the following: [a-zA-Z0-9_]
+
   const onlyValidCharacters = domain.match(validCharactersRegex);
+  // And then .match is used to see if there are any instances of the regex above in our domain
 
   const lastPortion = domain.split(".")[domain.split(".").length - 1];
+  // Here we separate the last portion of a domain (ex: gmail.com) splitting at the ".", which would result in an array -> ["gmail","com"]
+  // Then we save the last value of the array to the variable
 
   return !onlyValidCharacters?.length && lastPortion.length >= 2 ? true : false;
+  // Checking if onlyValidCharacters has length and negating it since if it hasn't a length value it means that the invalid characters are not present in the domain;
+  // Then we check if the last portion of the domain is equal or higher than 2
+  // If both are true we have a valid domain it returns true, if any of them is false the domain is invalid and it returns false
 };
 
 const isUsernameValid = (username: string) => {
   const validCharactersRegex = /[^\w.-]|([_.-][^\w{1}])/g;
+  // In the left side of the | in the above regex it matches all characters not in the group;
+  // \w is equivalent to the following: [a-zA-Z0-9_]
+  // In the right side we check to see if there are any instances where _ . - are not followed by a letter or number
   const onlyValidCharacters = username.match(validCharactersRegex);
 
   return !onlyValidCharacters?.length ? true : false;
+  // If any of the conditions above are matched it means the username is invalid and it returns false
 };
 
+// This function gets the current time and returns a string in a specific format (YYYY/MM/DD HH:mm:ss)
 const getDatestring = () => {
   const now = new Date();
 
@@ -60,10 +85,14 @@ const getDatestring = () => {
   const hours = String(now.getHours()).padStart(2, "0");
   const minutes = String(now.getMinutes()).padStart(2, "0");
   const seconds = String(now.getSeconds()).padStart(2, "0");
+  // We are using padStart to ensure that all values are full 2 digit numbers (ex: forcing 2022/6/1 which would be todays date -> 2022/06/01)
+  // To get the month value we add 1 since javascript uses 0 as January
 
   return `${year}/${month}/${day} ${hours}:${minutes}:${seconds}`;
+  // Then we just combine all the variables in a single string using the correct separators
 };
 
+// This function is just to group all function calls in a single one
 export const cleanEmail = (email: string) => {
   const emailWithoutComments = cleanComments(email);
   const emailWithoutPlus = cleanPlusSign(emailWithoutComments);
@@ -71,6 +100,7 @@ export const cleanEmail = (email: string) => {
   const emailWithoutWhitespace = cleanWhiteSpace(emailWithoutDotsBeforeAt);
 
   const [username, domain] = emailWithoutWhitespace.split("@");
+  // Using deconstruction and split to separate username and domain
 
   const emailReturn: ReturnInterface = {
     raw_email: email,
